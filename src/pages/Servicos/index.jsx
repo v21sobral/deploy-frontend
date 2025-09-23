3cc2fa62676b0265522f4a8830175f137dadf2c8
21Sobral <v21sobral@gmail.com>
2025-09-22 21:12:27 -0300

    Resident Security fase 2

--- src/pages/Servicos/index.jsx
+++ src/pages/Servicos/index.jsx
@@ -10,7 +10,7 @@ function Servicos() {
             <p>Comodidade e segurança na palma da mão</p>
 
             <div className='qrcode'>
-                <img src="src\assets\qrcode.png" alt="qrcode" />
+                <img src={hero} alt="qrcode" />
                 <h3>Contamos com a tecnologia de qrcode para facilitar o seu dia-a-dia dando mais segurança e rapidez nas suas entregas. A tecnologia de Qrcode já faz parte do nosso dia-a-dia e entegrada ao serviço de entregas tudo fica mais fácil.</h3>
             </div>
 
