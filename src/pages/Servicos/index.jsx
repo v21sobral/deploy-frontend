557e78a70eaa6b20b8a3403d3b157ccd1f367cbc
21Sobral <v21sobral@gmail.com>
2025-09-22 22:18:00 -0300

    Resident Security fase 2 import qrcode

--- src/pages/Servicos/index.jsx
+++ src/pages/Servicos/index.jsx
@@ -9,7 +9,7 @@ function Servicos() {
             <h1>Serviços de gestão de entregas</h1>
             <p>Comodidade e segurança na palma da mão</p>
 
-            <div className='qrcode'>
+            <div className='qrcodez'>
                 <img src={qrcodeImg} alt="qrcode" />
                 <h3>Contamos com a tecnologia de qrcode para facilitar o seu dia-a-dia dando mais segurança e rapidez nas suas entregas. A tecnologia de Qrcode já faz parte do nosso dia-a-dia e entegrada ao serviço de entregas tudo fica mais fácil.</h3>
             </div>
